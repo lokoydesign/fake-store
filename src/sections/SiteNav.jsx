5c3b056efe3045039c994e759242f49a5e88e6cf
Kniffen <kennethlokoy@gmail.com>
2022-07-05 18:24:31 +0200

    Update SiteNav.jsx

--- src/sections/SiteNav.jsx
+++ src/sections/SiteNav.jsx
@@ -1,69 +1,119 @@
 import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart, faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark, faHeart, faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 import { ENDPOINTS } from '../constants'
 
 export default function SiteNav() {
   const [ categories, setCategories ] = useState([])
+  const [ isMenuVisible, setIsMenuVisible ] = useState(false)
+  const cart = useSelector(state => state.cart)
 
   useEffect(function() {
     fetch(ENDPOINTS.CATEGORIES)
       .then(res => res.json())
       .then(setCategories)
       .catch(console.error)
-  })
+  }, [])
 
   if (1 > categories.length) return
 
   return (
     <StyledNavContainer>
-      <StyledNav>
-        <StyledNavLink to="/">All products</StyledNavLink>
+      <StyledToggleIcon icon={faBars} onClick={() => setIsMenuVisible(true)} />
+      
+      <StyledNav isMenuVisible={isMenuVisible}>
+        <StyledToggleIcon icon={faXmark} onClick={() => setIsMenuVisible(false)} />
+        <StyledNavLink to="/">Store</StyledNavLink>
         {categories.map((cat, i) => <StyledNavLink key={`category-${i}`}to={`/category/${encodeURIComponent(cat)}`}>{cat}</StyledNavLink>)}
         <StyledSaleLink to="/sale">Sale</StyledSaleLink>
       </StyledNav>
+
       <StyledStoreNav>
-        <StyledNavIconLink to="#"><FontAwesomeIcon icon={faHeart} /></StyledNavIconLink>
-        <StyledNavIconLink to="#"><FontAwesomeIcon icon={faUser} /></StyledNavIconLink>
-        <StyledNavCartLink to="#"><FontAwesomeIcon icon={faCartShopping} /></StyledNavCartLink>
+        <StyledNavIconLink to="/wishlist"><FontAwesomeIcon icon={faHeart} /></StyledNavIconLink>
+        <StyledNavIconLink to="/account"><FontAwesomeIcon icon={faUser} /></StyledNavIconLink>
+        <StyledNavCartLink to="/cart" count={cart.items.length}><FontAwesomeIcon icon={faCartShopping} /></StyledNavCartLink>
       </StyledStoreNav>
     </StyledNavContainer>
   )
 }
 
 const StyledNavContainer = styled.div`
+  position: relative;
   display: flex;
   gap: 1em;
-  padding: 1.5em 1em;
+  padding: 1em;
   border-block-end: .128em solid var(--color-blue);
   justify-content: space-between;
-
+  align-items: center;
+  
   @media (min-width: 82em) {
     padding-inline: calc((100% - 80em) / 2);
   }
 `
 
+const StyledToggleIcon = styled(FontAwesomeIcon)`
+  font-size: 1.4rem;
+  cursor: pointer;
+  color: var(--color-black);
+
+  @media (min-width: 48em) {
+    display: none;
+  }
+`
+
 const StyledNav = styled.nav`
-  display: flex;
-  gap: 1em;
+  background-color: var(--color-white);
+  display: ${props => props.isMenuVisible ? 'flex' : 'none'};
+  flex-direction: column;
+  align-items: center;
+  gap: 2em;
+  position: fixed;
+  inset-block-start: 0;
+  inset-inline-start: 0;
+  width: 100vw;
+  height: 100vh;
+  padding: 1em;
   text-transform: uppercase;
+  font-size: 1.4rem;
   font-weight: 600;
+  z-index: 100;
+  overflow-y: auto;
+
+  @media (min-width: 48em) {
+    background-color: transparent;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 1em;
+    padding: 0;
+    position: relative;
+    inset: initial;
+    width: initial;
+    height: initial;
+    font-size: 1rem;
+  }
 `
 
 const StyledStoreNav = styled.nav`
   display: flex;
   gap: 1em;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
 `
 
 const StyledNavLink = styled(NavLink)`
   color: var(--color-black);
 
+  &:hover,
   &.active {
     color: var(--color-blue);
+  }
+
+  &:hover {
+    text-decoration: none;
   }
 `
 
@@ -79,9 +129,14 @@ const StyledNavCartLink = styled(StyledNavIconLink)`
   position: relative;
 
   &::after {
+    display: ${props => props.count > 0 ? 'block' : 'none'};
     background-color: var(--color-blue);
     color: var(--color-white);
-    content: '50';
+    content: ${props => `"${props.count}"`};
+    width: 2.5em;
+    height: 2.5em;
+    line-height: 1.5em;
+    text-align: center;
     padding: .25em;
     font-size: .5rem;
     font-weight: 700;
