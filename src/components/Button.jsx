e92d8de097596388683d2bf64a8712a4cd3c83e5
Kniffen <kennethlokoy@gmail.com>
2022-07-05 17:47:45 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components'
 
-export default function Button({ className, text }) {
+export default function Button({ className, onClick, text }) {
   return (
-    <StyledButton className={className}>{text}</StyledButton>
+    <StyledButton className={className} onClick={onClick}>{text}</StyledButton>
   )
 }
 
 const StyledButton = styled.button`
   background-color: var(--color-blue);
   color: var(--color-white);
+  font-size: 1rem;
   font-weight: 500;
   padding: 1em;
   border: none;
