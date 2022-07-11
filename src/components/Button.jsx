df2c8b6822099f58d367ab5359ad7b7f8cb114eb
Kniffen <kennethlokoy@gmail.com>
2022-07-11 16:34:44 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,20 +2,20 @@ import { Link } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
 
-export default function Button({ className, to, onClick, children }) {
+export default function Button({ className, isPrimary = true, to, onClick, children }) {
 
   if (to)
-    return (<StyledLink className={className} to={to}>{children}</StyledLink>)
+    return (<StyledLink className={className} isPrimary={isPrimary} to={to}>{children}</StyledLink>)
 
   return (
-    <StyledButton className={className} onClick={onClick}>{children}</StyledButton>
+    <StyledButton className={className} isPrimary={isPrimary} onClick={onClick}>{children}</StyledButton>
   )
 }
 
 
 const styles = css`
-  background-color: var(--color-blue);
-  color: var(--color-white);
+  background-color: ${props => props.isPrimary ? 'var(--color-blue)' : 'var(--color-gray--400)'};
+  color: ${props => props.isPrimary ? 'var(--color-white)' : 'var(--color-black)'};
   font-size: 1rem;
   padding: 1em;
   border: none;
@@ -26,7 +26,7 @@ const styles = css`
   text-align: center;
 
   &:hover {
-    filter: brightness(110%);
+    filter: ${props => props.isPrimary ? 'brightness(110%)' : 'brightness(90%)'};
   }
 `
 
